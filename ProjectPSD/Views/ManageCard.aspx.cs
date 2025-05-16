ec106cd98475e0735814a8f0e90a05bf267bfe63
F4E12 <76392444+F4E12@users.noreply.github.com>
2025-05-16 18:54:47 +0700

    add Manage Cart View Page

---
+++ ProjectPSD/Views/ManageCard.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ProjectPSD.Views
+{
+    public partial class ManageCard : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ManageCardGV_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ManageCardGV_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+
+        }
+
+        protected void ManageCardGV_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+
+        }
+    }
+}
